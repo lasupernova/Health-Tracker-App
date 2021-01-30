b138ce8cb7c301f021333c46fa47357df29ba316
lasupernova <gabrilasuno@gmail.com>
2021-01-30 22:25:25 +0100

    added content to mood tab

--- style/widget_style.py
+++ style/widget_style.py
@@ -14,16 +14,17 @@ class Style():
         # ----- Colors ----- 
         COL_PRIM = "#585278"
         COL_SEC = "#6f6898"
-        COL_LIGHT_BG = "#fff"
+        COL_LIGHT_BG = "#ebe4ea" #"fff"
         COL_LIGHT_TXT = "#eee"
         COL_DARK_TXT = "#311a80"
         COL_HIGHLIGHT = "#e3007d"
+
 
         # create custom style
         style = ttk.Style(self.controller)
         style.theme_use("clam")
         style.configure("Timer.TFrame", background=COL_LIGHT_BG) #style 1
-        style.configure("Background.TFrame", background=COL_PRIM) #style 2
+        style.configure("Background.TFrame", background=COL_LIGHT_BG) #style 2
         style.configure("Timer.TFrame", background=COL_LIGHT_BG) #style 3 
         style.configure("TimerText.TLabel", background=COL_LIGHT_BG, foreground=COL_DARK_TXT, font="Courier 38") #style 4
         style.configure("LightText.TLabel", background=COL_PRIM, foreground=COL_LIGHT_TXT) #style 5
--- tracker.py
+++ tracker.py
@@ -42,11 +42,11 @@ class InputWindow(tk.Tk):
     # ----- Tabs -----
 
         # initiate ttk.Notebook as parent for tabs
-        tabControl = ttk.Notebook(self, style="TimerText.TLabel")#, style="Custom.TNotebook.Tab")
+        tabControl = ttk.Notebook(self)#, style="Custom.TNotebook.Tab")
 
         # create tabs
-        mood_tab = ttk.Frame(tabControl, style="Background.TFrame")
-        health_tab = ttk.Frame(tabControl, relief = tk.SUNKEN)
+        mood_tab = ttk.Frame(tabControl)
+        health_tab = ttk.Frame(tabControl) #, relief = tk.SUNKEN
         sleep_tab = ttk.Frame(tabControl)
         food_tab = ttk.Frame(tabControl)
         fitness_tab = ttk.Frame(tabControl)
@@ -66,8 +66,47 @@ class InputWindow(tk.Tk):
         tabControl.pack(expand=1, fill="both")
 
     # ----- Labels ----- 
-        ttk.Label(mood_tab,  text ="Mood Fields here! \n 🡓", font={'size':12}).grid(column = 0,  row = 0, padx = 30, pady = 30)  
+        ttk.Label(mood_tab,  text ="Mood Fields here! \n 🡓", font={'size':12}).pack()#.grid(column = 0,  row = 0, padx = 30, pady = 30)  
         ttk.Label(health_tab,  text ="Health Fields here! \n 🡓").grid(column = 0,  row = 0, padx = 30, pady = 30)  
+
+    # ----- Options -----
+
+        # list of all options to add as checkboxes
+        self.mood_options = ['angry', 'anxious', 'calm', 'content', 'depressed', 'emotional', 'energetic', 'excited', 'frustrated', 'happy', 'hyper', 'moody', 'motivated', 'relaxed', 'sad', 'sensitive', 'stressed', 'tired']
+        # initiate dict to save tk.Stringvar-objects to sabe checkbox states in
+        self.mood_selections = {}
+        # iterate over options and save tk.Stringvar object as value
+        for option in self.mood_options:
+            self.mood_selections[option] = tk.StringVar(value=0)
+
+
+    #  ----- Entry -----
+
+        # iterate over options nand create checkbutton for each option
+        for option in self.mood_selections:
+            # frame = ttk.Frame(mood_tab).pack(fill="both", anchor="w")
+            ttk.Checkbutton(mood_tab,
+                            text=option,
+                            command=lambda x=option: self.check_mood(x), #lambda command refering to method in order to be able to pass current option name as variable
+                            variable=self.mood_selections[option]).pack(anchor="w")
+
+    # ----- Buttons -----
+        test = ttk.Button(
+            mood_tab,
+            command=self.print_all_selected,
+            text="Print Selection"
+        )
+        test.pack()
+
+    # method printing current checkbutton state when clicked
+    def check_mood(self, option):
+        # print(self.angry_check.state())
+        print(self.mood_selections[f"{option}"].get())
+
+    # method printing all current checkbutton states
+    def print_all_selected(self):
+        for mood in self.mood_selections:
+            print(mood,": ", self.mood_selections[mood].get())
 
 # ----- run app -----
 if __name__ == '__main__':
