ee81fb6c4dda8b6a1f9326d27a2469e648a53fa8
lasupernova <gabrilasuno@gmail.com>
2021-02-06 21:00:54 +0100

    Removed all files that are in the .gitignore

--- frames/analysis/plots/test2.py
+++
@@ -1,6 +0,0 @@
-import plotly.express as px
-
-gapminder = px.data.gapminder()
-fig = px.scatter(gapminder.query("year==2007"), x="gdpPercap", y="lifeExp", size="pop", color="continent",
-           hover_name="country", log_x=True, size_max=60)
-fig.show()
