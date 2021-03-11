0248ff755eafeda262e732d4184bca2a2e74bce9
lasupernova <gabrilasuno@gmail.com>
2021-03-11 22:21:40 +0100

    added main frame, binding together other frames and enabling switching between frames

---
+++ frames/main_frame.py
@@ -0,0 +1,57 @@
+# ----- import libraries and  modules ---
+import tkinter as tk
+from tkinter import ttk
+import tkinter.font as tkFont
+import os
+from PIL import ImageTk, Image
+import datetime
+import sys
+# from database.connections import db_transact #NOTE: use python -m frames.login_frame in order to circumvent relative import issue
+from frames.signup_frame import SignupWindow
+from frames.login_frame import LoginWindow
+
+#  ----- class inheriting from tk.Tk -----
+class MainWindow(tk.Tk):
+    #  ----- initialize -----
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        #  ----- Frames ----- 
+
+        # save a ttk frame - object in a variable named "container" 
+        container = ttk.Frame(self) 
+        container.grid() #position the frame in the parent widget in a grid
+        container.columnconfigure(0, weight=1)
+
+        # create dictionary to keep track of frames
+        self.frames = dict()
+
+        # add timer frame that is placed within "container"
+        self.login_frame = LoginWindow(container, lambda: self.switch_frame(SignupWindow)) #initiate Timer-class and pass self as the controller
+        self.login_frame.grid(row=0, column=0, sticky="NESW") #configure timer frame placed in the first row and first column and to fill the entire frame ("container")
+
+        # add settings frame 
+        self.signup_frame = SignupWindow(container, lambda: self.switch_frame(LoginWindow))
+        self.signup_frame.grid(row=0, column=0, sticky="NESW") 
+
+        # add both frames to dict
+        self.frames[LoginWindow] = self.login_frame
+        self.frames[SignupWindow] = self.signup_frame
+
+        # start with timer_frame in front
+        self.switch_frame(LoginWindow)
+
+    # ----- function that brings frame on the back to the front -----
+    def switch_frame(self, container):
+        # indicate which frame to bring to front
+        frame = self.frames[container]
+        #brings indicated frame to the front
+        frame.tkraise() 
+        print("WORKS!")
+
+
+# ----- run app -----
+if __name__ == '__main__':
+    app = MainWindow() 
+
+    app.mainloop()
