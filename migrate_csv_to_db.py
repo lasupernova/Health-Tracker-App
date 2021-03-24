64ff717a34924a1e3f8a0eacbbaa3d185fd9f277
lasupernova <gabrilasuno@gmail.com>
2021-03-24 20:20:55 +0100

    finalized script to migrate data from .csv-file to database

--- migrate_csv_to_db.py
+++ migrate_csv_to_db.py
@@ -115,6 +115,123 @@ uid_oi = cur.fetchone()[0]
 #         cur.execute(mood_query, (angry, anxious, calm, content, depressed, emotional, energetic, excited, frustrated, happy, hyper, moody, motivated, relaxed, sad, sensitive, stressed, tired, date))
 #         conn.commit()
 
+# health = df.health
+
+# print(sorted(list(health.columns)))
+
+# num_cols = ('%s, '*22)
+
+# health_query = f'''INSERT INTO health (acidity, backpain, bloating, breakouts, chestpain, constipation, defecation, diarrhea, dizziness, hard_stool, headache, indigestion, medication, nausea, numbness, other_symptoms, palpitations, panic_attack, breathlessness, sick, stomachpain, date, user_id)
+#                 VALUES ({num_cols} {uid_oi})'''
+
+# for idx in health.index:
+#     date = idx
+#     acidity = bool(health.loc[idx, 'acidity']) 
+#     backache = bool(health.loc[idx, 'backache'])  
+#     bloating = bool(health.loc[idx, 'bloating'])  
+#     breakouts= bool(health.loc[idx, 'breakouts'])  
+#     chestpain = bool(health.loc[idx, 'chestpain'])  
+#     constipation = bool(health.loc[idx, 'constipation'])  
+#     defecation = bool(health.loc[idx, 'defecation'])  
+#     diarrhea = bool(health.loc[idx, 'diarrhea'])  
+#     dizziness = bool(health.loc[idx, 'dizziness'])  
+#     hard_stool = bool(health.loc[idx, 'hard stool'])  
+#     headache = bool(health.loc[idx, 'headache'])  
+#     indigestion = bool(health.loc[idx, 'indigestion'])  
+#     medication = str(health.loc[idx, 'medication']) if str(health.loc[idx, 'medication']) != '[nan]' else None
+#     nausea = bool(health.loc[idx, 'nausea'])  
+#     numbness = str(health.loc[idx, 'numbness']) if str(health.loc[idx, 'numbness']) != '[nan]' else None
+#     other_symptoms = bool(health.loc[idx, 'other symptoms']) if str(health.loc[idx, 'other symptoms']) != '[nan]' else None
+#     palpitations = bool(health.loc[idx, 'palpitations'])  
+#     panic_attack = bool(health.loc[idx, 'panic attack'])  
+#     breathlessness = bool(health.loc[idx, 'shortness of breath']) 
+#     sick = bool(health.loc[idx, 'sick?']) if str(health.loc[idx, 'sick?']) != '[nan]' else None
+#     stomachpain = bool(health.loc[idx, 'stomachpain'])
+
+#     with conn.cursor() as cur:
+#         cur.execute(health_query, (acidity, backache, bloating, breakouts, chestpain, constipation, defecation, diarrhea, dizziness, hard_stool, headache, indigestion, medication, nausea, numbness, other_symptoms, palpitations, panic_attack, breathlessness, sick, stomachpain, date))
+#         conn.commit()
+
+# period = df.period
+
+# print(sorted(list(period.columns)))
+
+# num_cols = ('%s, '*10)
+
+# # print(period.spotting.unique())
+
+# period_query = f'''INSERT INTO period (cramps, cramps_level, cycle_day, infection, ovulation, period, intercourse, spotting, spotting_level, date, user_id)
+#                 VALUES ({num_cols} {uid_oi})'''
+
+# for idx in period.index:
+#     date = idx
+#     cramps = bool(period.loc[idx, 'cramps']) 
+#     cramps_level = 2 if cramps==1 else 0
+#     cycle_day = int(period.loc[idx, 'cycle day']) 
+#     infection = str(period.loc[idx, 'infection']) if str(period.loc[idx, 'infection']) != '[nan]' else None
+#     ovulation = bool(period.loc[idx, 'ovulation'])
+#     period_ = bool(period.loc[idx, 'period'])
+#     intercourse = bool(period.loc[idx, 'sex'])
+#     spotting = bool(period.loc[idx, 'spotting'])
+#     spotting_level = 2 if spotting==1 else 0
+
+#     with conn.cursor() as cur:
+#         cur.execute(period_query, (cramps, cramps_level, cycle_day, infection, ovulation, period_, intercourse, spotting, spotting_level, date))
+#         conn.commit()
+
+# lt = df.longterm
+
+# print(sorted(list(lt.columns)))
+
+# num_cols = ('%s, '*6)
+
+# # print(period.spotting.unique())
+
+# lt_query = f'''INSERT INTO longterm (anatomical, climate, hormonal, nutritional, social, date, user_id)
+#                 VALUES ({num_cols} {uid_oi})'''
+
+# for idx in lt.index:
+#     date = idx
+#     anatomical = str(lt.loc[idx, 'anatomical']) if str(lt.loc[idx, 'anatomical']) != '[nan]' else None
+#     climate = str(lt.loc[idx, 'climate']) if str(lt.loc[idx, 'climate']) != '[nan]' else None
+#     hormonal = str(lt.loc[idx, 'hormonal']) if str(lt.loc[idx, 'hormonal']) != '[nan]' else None
+#     nutritional = str(lt.loc[idx, 'nutritional']) if str(lt.loc[idx, 'nutritional']) != '[nan]' else None
+#     social = str(lt.loc[idx, 'social']) if str(lt.loc[idx, 'social']) != '[nan]' else None
+
+#     with conn.cursor() as cur:
+#         cur.execute(lt_query, (anatomical, climate, hormonal, nutritional, social, date))
+#         conn.commit()
+
+# sleep = df.sleep
+
+# # print(sleep.REM.unique())
+
+# num_cols = ('%s, ' * 11) 
+
+# sleep_query = f'''INSERT INTO sleep (sleep, REM, awake, deep_sleep, light_sleep, sleep_score, insomnia, freq_wakes, sleep_meds, tz_change, date, user_id)
+#                 VALUES ({num_cols} {uid_oi})'''
+
+# for idx in sleep.index: 
+#     date = idx
+#     sleep_ = float(sleep.loc[idx, 'sleep'])
+#     rem = 0.01 if str(sleep.loc[idx, 'REM'])=='nan' else 0.01 if float(sleep.loc[idx, 'REM'])== 0.0 else float(sleep.loc[idx, 'REM'])
+#     awake = 0.01 if str(sleep.loc[idx, 'awake'])=='nan' else float(sleep.loc[idx, 'awake'])
+#     deep_sleep = 0.01 if str(sleep.loc[idx, 'deep sleep'])=='nan' else float(sleep.loc[idx, 'deep sleep'])
+#     light_sleep = 0.01 if str(sleep.loc[idx, 'light sleep'])=='nan' else float(sleep.loc[idx, 'light sleep'])
+#     try:
+#         sleep_score = None if str(sleep.loc[idx, 'sleep score'])=='nan' else int(sleep.loc[idx, 'sleep score'])
+#     except:
+#         sleep_score = 0
+#     insomnia = bool(sleep.loc[idx, 'insomnia'])
+#     freq_wakes = bool(sleep.loc[idx, 'frequent wakeups'])
+#     sleep_meds = str(sleep.loc[idx, 'sleep medication']) if str(sleep.loc[idx, 'sleep medication']) != '[nan]' else None
+#     tz_change = bool(sleep.loc[idx, 'timezone change'])
+
+#     # print(sleep_, rem, awake, deep_sleep, light_sleep, sleep_score, insomnia, freq_wakes, sleep_meds, tz_change, date)
+
+#     with conn.cursor() as cur:
+#         cur.execute(sleep_query, (sleep_, rem, awake, deep_sleep, light_sleep, sleep_score, insomnia, freq_wakes, sleep_meds, tz_change, date))
+#         conn.commit()
 
 conn.close()
     
