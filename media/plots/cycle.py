fe22c0630387b261f39ac45f4c3d23c15f42d38f
lasupernova <gabrilasuno@gmail.com>
2021-04-02 13:48:17 +0200

    modified xaxis formatting based on index length and moved into separate function

--- media/plots/cycle.py
+++ media/plots/cycle.py
@@ -1,15 +1,48 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 import seaborn as sns
 from pandas import Timestamp
 from matplotlib import colors as mcolors
+import matplotlib.ticker as mticker
 import matplotlib
 import datetime
 import math
 import os
 import database.connections.db_transact as db_transact
+
+
+def custom_locator(index, ax, small_fonts):
+    '''
+    Function taking index and matplotlib axis and creating formatted xaxis ticks and labels for specified axis (based on index length)
+
+    Parameter: 
+        index: (DateTime)-Index
+        ax: 
+    '''
+    print(f"AX {type(ax)}:")
+    idx_length = len(index)
+
+    if idx_length < 30:
+        pass
+
+    elif idx_length < 365:
+        ax.xaxis.set_major_locator(matplotlib.dates.MonthLocator())
+        ax.xaxis.set_minor_locator(matplotlib.dates.DayLocator((1,7,14,21,28)))
+
+        ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("\n%b"))
+        ax.xaxis.set_minor_formatter(matplotlib.dates.DateFormatter("%d"))
+
+    else:
+        ax.xaxis.set_major_locator(matplotlib.dates.YearLocator())
+        ax.xaxis.set_minor_locator(matplotlib.dates.MonthLocator((1,3,5,7,9,11)))
+
+        ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("\n%Y"))
+        ax.xaxis.set_minor_formatter(matplotlib.dates.DateFormatter("%b"))
+
+    ax.tick_params(axis="x", which="minor", rotation=90,labelsize=small_fonts)  #changed from: plt.setp(ax.get_xticklabels(), rotation=0, ha="center")
+    ax.tick_params(axis="x", which="major", length=8)
 
 
 def plot_cycle(user, date): 
@@ -18,7 +51,7 @@ def plot_cycle(user, date):
     '''
 
     end_date = date - datetime.timedelta(weeks=13)
-    start_date = end_date - datetime.timedelta(weeks=6)
+    start_date = end_date - datetime.timedelta(weeks=10)
     
     columns=['cycle_day', 'date']
     table = 'period'
@@ -30,7 +63,7 @@ def plot_cycle(user, date):
     # create dataframe from data
     print(columns[:-1])
     df = pd.DataFrame(data=data_values, index=date, columns=columns[:-1])
-    print(df)
+    print(df.index)
 
     #find local maxima
     locMax = df['cycle_day'][(df['cycle_day'].shift(1) < df['cycle_day']) & (df['cycle_day'].shift(-1) < df['cycle_day'])]
@@ -69,7 +102,7 @@ def plot_cycle(user, date):
     ax.axhline(y=cycleLengthMean,xmin=0,xmax=0.98,color='pink',ls='-.',lw=0.7)
     total_days = len(df.index)
     lastDate = df.index.max()+datetime.timedelta(days=total_days/20) #add days for readability
-    ax.text(x=lastDate, y=cycleLengthMean-0.3, s= f'mean cycle length',color='pink',size=big_fonts)
+    ax.text(x=lastDate, y=cycleLengthMean-0.3, s= f'mean cycle length',color='pink',size=big_fonts)  #NOTE: all "plt" changed to "ax" --> code behind stayed the same and works
     ax.axhline(y=ovulation,xmin=0.04,xmax=0.98,color='purple',lw=0.5)
     ax.text(x=lastDate, y=ovulation-0.5, s= f'Ovulation (Cycle Day {ovulation})',color='purple',size=big_fonts)
     ax.axhspan(ymin=fertile_start, ymax=fertile_trail, xmin=0.04, xmax=0.98,color='purple',alpha=0.1, zorder=-1)
@@ -77,17 +110,15 @@ def plot_cycle(user, date):
     ax.axhspan(ymin=fertile_high, ymax=ovulation, xmin=0.04, xmax=0.98,color='purple',alpha=0.2, zorder=-1)
     ax.text(x=lastDate, y=fertile_high-0.3, s= f'cycle day {fertile_high}',color='purple',size=small_fonts,alpha=0.5)
 
-    # ax.xaxis.set_major_locator(matplotlib.dates.YearLocator())
-    # ax.xaxis.set_minor_locator(matplotlib.dates.MonthLocator((1,3,5,7,9,11)))
 
-    # ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("\n%Y"))
-    # ax.xaxis.set_minor_formatter(matplotlib.dates.DateFormatter("%b"))
-    # plt.setp(ax.get_xticklabels(), rotation=0, ha="center")
-    # # plt.legend()
+    custom_locator(df.index, ax, small_fonts)
 
-    # sns.despine(left=True, bottom = True)
-    # plt.tight_layout()
+    for axis in ax.spines:
+        ax.spines[axis].set_visible(False)  #changed from: sns.despine(left=True, bottom = True)
+    fig.tight_layout()
 
     # plt.show()
     return fig
     quit()
+
+
