07fc59b33bab00a5c6d6fa792ce0203bd03395df
lasupernova <gabrilasuno@gmail.com>
2021-04-24 19:35:03 +0200

    modified wrapper.py to enable full word cloud generation functionality integrated with ddatabase querying

--- wrapper.py
+++ wrapper.py
@@ -4,4 +4,6 @@ wrapper to avoid relative path issues when using modules in different folders
 import media.plots.food as food
 
 if __name__ == "__main__":
-    food.create_cloud('ywb', 'test')
+    data, dates = food.get_data()
+    data_dict = food.get_counts_from_list(data)
+    food.create_cloud(data_dict, "wrapper")
