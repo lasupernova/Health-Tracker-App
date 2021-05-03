c9f9fe1fcec41e5f99517ea944af610fc1a2e5d0
lasupernova <gabrilasuno@gmail.com>
2021-05-03 22:09:50 +0200

    created helper.py to store functions frequently reused across different parts and scripts of the app

---
+++ helpers.py
@@ -0,0 +1,37 @@
+def change_color(self, widget):
+    '''
+    Creates illusion of widget "blinking" by gradually switching colors to background color and back to original widget text color.
+    
+    NOTE1: running .after() in a loop (e.g. for) does not work, as the loop runs while after-round is still waiting
+    NOTE2: subsequent .after()-calls, need to have increasing delays, as the first after-call delay time nis substracted from the 2nd one, the 2nd from the 3rd etc.
+    --> solutions:
+        1) extend tint-list by how often should be ran through
+        2) call .enumerate() and multiply the enumerator by the desired time for each color
+    '''
+    def _color_text(widget, color):
+        ''' Change font color of widget to "color" '''
+        widget.config(fg=color)
+
+    tints_of_blue = ["#0000FF", "#1919ff", "#3232ff", "#4c4cff", "#6666ff", "#7f7fff", "#9999ff", "#b2b2ff", "#ccccff", "#e5e5ff"]  #list of blue with decreasing saturation
+    tints_of_blue += tints_of_blue[::-1]  #append reversed list to list
+    tints_of_blue *= 3
+    time_per_col = int(4000/len(tints_of_blue))   #each "blink" should take 60 seconds
+
+
+    for n, tint in enumerate(tints_of_blue):
+        self.after(time_per_col*n, _color_text, widget, tint)
+
+
+def changeOnHover(button, fgColorOnHover, fgColorOnLeave, bgColorOnHover="#DCDAD5", bgColorOnLeave="#DCDAD5"): 
+    def _modify(e, fgcol, bgcol):
+        button.config(fg=fgcol)
+        button.config(bg=bgcol)
+    # background on cursor entering widget 
+    button.bind("<Enter>", 
+                func=lambda e, fgcol=fgColorOnHover, bgcol=bgColorOnHover: _modify(e, fgcol, bgcol)
+                )  
+        
+    # background color on cursor leaving widget 
+    button.bind("<Leave>", 
+                func=lambda e, fgcol=fgColorOnLeave, bgcol=bgColorOnLeave: _modify(e, fgcol, bgcol)
+                )  
