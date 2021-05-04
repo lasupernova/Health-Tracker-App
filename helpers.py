dc253b97beabbbd09463c786b2b435339e76b268
lasupernova <gabrilasuno@gmail.com>
2021-05-04 20:30:30 +0200

    modified changeOnHover() to enable blinking functionality by calling change_color()

--- helpers.py
+++ helpers.py
@@ -1,4 +1,4 @@
-def change_color(self, widget):
+def change_color(widget):
     '''
     Creates illusion of widget "blinking" by gradually switching colors to background color and back to original widget text color.
     
@@ -17,18 +17,28 @@ def change_color(self, widget):
     tints_of_blue *= 3
     time_per_col = int(4000/len(tints_of_blue))   #each "blink" should take 60 seconds
 
-
     for n, tint in enumerate(tints_of_blue):
-        self.after(time_per_col*n, _color_text, widget, tint)
+        widget_after_id = widget.after(time_per_col*n, _color_text, widget, tint)
+        RECORD_BUTTON_BLINK[widget] = widget_after_id
 
+
+ 
+global RECORD_BUTTON_BLINK  #initiate dict that stores after_ids and cancels id for current widget if it is still running
+RECORD_BUTTON_BLINK = {}
 
 def changeOnHover(button, fgColorOnHover, fgColorOnLeave, bgColorOnHover="#DCDAD5", bgColorOnLeave="#DCDAD5"): 
-    def _modify(e, fgcol, bgcol):
+    def _modify(e, fgcol, bgcol, blink=0):
+        if button in RECORD_BUTTON_BLINK.keys(): 
+            button.after_cancel(RECORD_BUTTON_BLINK[button])  #if previous after still running for this widget - cancel it, so that blinking frequency is not changed
+        
+        if (blink != 0) and ('CLICK ME!' in button.cget('text')):  #tun only if blink is set to one and if button text does not contain 'HIT ME!' substring"
+            change_color(button)
         button.config(fg=fgcol)
         button.config(bg=bgcol)
+    
     # background on cursor entering widget 
     button.bind("<Enter>", 
-                func=lambda e, fgcol=fgColorOnHover, bgcol=bgColorOnHover: _modify(e, fgcol, bgcol)
+                func=lambda e, fgcol=fgColorOnHover, bgcol=bgColorOnHover, blink=1: _modify(e, fgcol, bgcol, blink)
                 )  
         
     # background color on cursor leaving widget 
