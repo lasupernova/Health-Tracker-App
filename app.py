a73fd4d5a54b5a96b2917a2e1e0e858c7a50f8e0
lasupernova <gabrilasuno@gmail.com>
2021-05-05 22:19:37 +0200

    modified code for html.Div(id='tabs-content-example') to be on aseparate row using dbc.Row

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import dash
 from dash.dependencies import Input, Output
 import dash_html_components as html
 import dash_core_components as dcc
+import dash_bootstrap_components as dbc
 from entry import create_entry_fields
 from assets.entry_information import * 
 
@@ -14,7 +15,7 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div([
     # html.H1('Dash Tabs component demo'),
     html.Div([
-        dcc.Tabs(id="tabs", value='tab-1-example', children=[
+        dbc.Row([dcc.Tabs(className='row', id="tabs", value='tab-1-example', children=[
             dcc.Tab(id="mood",label='Mood', value='mood'),
             dcc.Tab(label='Health', value='health'),
             dcc.Tab(label='Food', value='food'),
@@ -22,34 +23,42 @@ app.layout = html.Div([
             dcc.Tab(label='Period', value='period'),
             dcc.Tab(label='Sleep', value='sleep'),
             dcc.Tab(label='Longterm', value='longterm'),
-        ],  vertical=False, parent_style={'float': 'left'}),
-        html.Div(id='tabs-content-example', style={'float': 'left', 'width': '1000'})
+        ],  vertical=False, parent_style={'float': 'left'})]),
+        dbc.Col([html.Div(id='tabs-content-example')])
     ])
 ])
 
 #  add tab content as tabs' children using call back
-@app.callback(Output('mood', 'children'),
+@app.callback(Output('tabs-content-example', 'children'),
               [Input('tabs', 'value')])
-def render_content(tab, mood_info=mood_info):
+def render_content(tab):
     if tab == 'mood':
+        checklist, sliders = create_entry_fields(mood_info)
         return dcc.Checklist(
-                            options=create_entry_fields(mood_info),
+                            options=checklist,
                             value=[]
                         )  
     elif tab == 'health':
-        return html.Div([
-            html.H3('Tab content 2'),
-            dcc.Graph(
-                id='graph-2-tabs',
-                figure={
-                    'data': [{
-                        'x': [1, 2, 3],
-                        'y': [5, 10, 6],
-                        'type': 'bar'
-                    }]
-                }
-            )
-        ])
+        checklist, sliders = create_entry_fields(health_info)
+        # for slider in sliders:
+        #     dcc.Slider(
+        #             min=-5,
+        #             max=10,
+        #             step=0.5,
+        #             value=-3
+        #         )  
+        return [
+            dcc.Checklist(
+                            options=checklist,
+                            value=[]
+                        ),
+            dcc.Slider(
+                    min=-5,
+                    max=10,
+                    step=0.5,
+                    value=-3
+                )  
+        ]
 
 
 
