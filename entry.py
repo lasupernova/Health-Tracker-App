bc3198604a9c6fece1d40cfc6c4b4980b2d5c310
lasupernova <gabrilasuno@gmail.com>
2021-05-05 22:18:45 +0200

    added slider info to return value

--- entry.py
+++ entry.py
@@ -1,5 +1,6 @@
 def create_entry_fields(section):
     checklist = []
+    sliders = []
     for entry in section:
         entry_name = [*entry][0]  #get param name
         entry_type = entry[entry_name]['type']
@@ -12,10 +13,14 @@ def create_entry_fields(section):
             from_ = entry[entry_name]['from']
             to_ = entry[entry_name]['to']
             increment = entry[entry_name]['increment']
+            slider = {'label':entry_label.capitalize(), 'min':from_, 'max':to_, 'step':increment}
+            sliders.append(slider)
+            if 'on_demand' in entry[entry_name].keys():
+                on_demand = entry[entry_name]['on_demand']
         elif entry_type == 'Entryfield':
             pass
         elif entry_type == 'MultipleChoice':
             selection = entry[entry_name]['selection_menu']
             on_demand = entry[entry_name]['on_demand']
 
-    return checklist
+    return checklist, sliders
