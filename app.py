f196fd3eb62e2894ca8c0faf07aa41e70a2102d0
lasupernova <gabrilasuno@gmail.com>
2021-05-11 22:23:26 +0200

    fixed bug: ensured length of (entry-)names, values and type lists are equal

--- app.py
+++ app.py
@@ -140,15 +140,22 @@ def insert_database(data, tab, user='gabri', date=datetime.now().date()):
 def send_to_db(tab, values, names, tab_to_db):
     # print('TAB TO DB: ', tab_to_db)  ##status of last tab (the one to send data to db for) is returned and saved
     if (tab != 'tab-1-example') and (tab_to_db != 'tab-1-example'):
-        info = {n['name']: v for n, v in zip(names, values) if tab_to_db in n['name']}  ##get only values when corresponding names contain category name as substring
+        info = {n['name']: v for n, v in zip(names, values) if n['name'].startswith(tab_to_db)}  ##get only values when corresponding names contain category name as substring
         name = [k for k in info.keys()]
+        print(">>>JOINT: ", len(info))
         values = [v for k, v in info.items()]
-        type_ = [n['type'] for n in names if tab_to_db in n['name']]
-        print(f"TYPE: {type_}")
+        type_ = [n['type'] for n in names if (n['name'].startswith(tab_to_db) and n['type'] != 'div')]  ##use startswith() instead of tab_to_db in n['name'], as 'health' also in 'food_unhealthy_food'
+        print(f"TYPES: {len(type_)}")
         to_db = {}
+        if len(n) != len(t) != len(v):  ##need to be of same lenght in order to successfully pass data to db
+                print("An Error Occured! Lengths of name, value and type lists are unequal!")
+                return "ERROR!"
         for n, v, t in zip(name, values, type_):  #zip all three if type is 'div' for one - continue (as that will be a div as oppossed to a dcc)
+
             if (t != 'div') and v:  #checklists that are not checked return empty list (if <empty_list> - returns False); other entryfields without entry will return None
-                entry_name = n.split('_')[1]  ##first part of n is the cotegory --> grab only relevant entry name like used in db
+                print(f"n: {n}")
+                entry_name = "_".join(n.split('_')[1:])  ##first part of n is the cotegory --> grab only relevant entry name like used in db
+                print(f">>>>>CONCATENATION: {entry_name}")
                 to_db[entry_name] = True if 'check' in t else v
                 print(f"{entry_name} : {1 if 'check' in t else v}")
 
