7fe6991ffc2535ce2dd6c2bf4404cd56608043ff
lasupernova <gabrilasuno@gmail.com>
2021-05-14 22:56:51 +0200

    improved code efficiency by moving date-picker outside of tabs-object and implementing only one date-picker object for all tabs

--- app.py
+++ app.py
@@ -33,19 +33,28 @@ app.layout = html.Div([
         message='',
         ),
     html.Div([
-        dbc.Row([dcc.Tabs(className='row', id="tabs", value='tab-1-example', children=[
-            dcc.Tab(id='mood_tab', label='Mood', value='mood'),
-            dcc.Tab(id='health_tab', label='Health', value='health'),
-            dcc.Tab(id='food_tab', label='Food', value='food'),
-            dcc.Tab(id='fitness_tab', label='Fitness', value='fitness'),
-            dcc.Tab(id='period_tab', label='Period', value='period'),
-            dcc.Tab(id='sleep_tab', label='Sleep', value='sleep'),
-            dcc.Tab(id='longterm_tab', label='Longterm', value='longterm'),
-        ],  vertical=False, parent_style={'float': 'left'})])
+        dbc.Row(
+            [dbc.Col([
+                dcc.Tabs(className='row', id="tabs", value='tab-1-example', children=[
+                dcc.Tab(id='mood_tab', label='Mood', value='mood'),
+                dcc.Tab(id='health_tab', label='Health', value='health'),
+                dcc.Tab(id='food_tab', label='Food', value='food'),
+                dcc.Tab(id='fitness_tab', label='Fitness', value='fitness'),
+                dcc.Tab(id='period_tab', label='Period', value='period'),
+                dcc.Tab(id='sleep_tab', label='Sleep', value='sleep'),
+                dcc.Tab(id='longterm_tab', label='Longterm', value='longterm'),
+            ],  vertical=False, parent_style={'float': 'left'})], width = 9),
+            dbc.Col([
+                        dcc.DatePickerSingle(
+                            id="date_picker",
+                            date=datetime.today().date()),
+                        html.Div(id="current_date", children=["BLUB"])
+                        ], width = "auto") 
 
-    ]),
+        ]),
     html.Div(id="values_to_db", children=['None'], style={'display':'block'}),
     html.Div(id="test", children=['None'], style={'display':'block'})
+    ])
 ])
 
 # def test():
@@ -79,13 +88,13 @@ def display_page(pathname):
                                 },
                             style = dict(display='inline-block')
                         )
-                        ], width = "auto"),
-                dbc.Col([
-                        dcc.DatePickerSingle(
-                            id={"name":k,"type":"startdate-input"},
-                            date=datetime.today().date()),
-                        html.Div(id={"name":k,"type":"picked_date"}, children=["TEST"])
-                        ], width = "auto") 
+                        ], width = "auto")
+                # dbc.Col([
+                #         dcc.DatePickerSingle(
+                #             id={"name":k,"type":"startdate-input"},
+                #             date=datetime.today().date()),
+                #         html.Div(id={"name":k,"type":"picked_date"}, children=["TEST"])
+                #         ], width = "auto") 
                     ])
             return_vals[k] = template
     return return_vals['mood'], return_vals['health'], return_vals['food'], return_vals['fitness'], return_vals['period'], return_vals['sleep'], return_vals['longterm']
@@ -111,10 +120,9 @@ def display_page(pathname):
 
 # toggle entryfields for checkboxed for which additional info is necessary
 @app.callback(Output({'name': MATCH, 'type': 'div'}, 'style'),
-              [Input({'name': MATCH, 'type': 'check_toggle'}, 'value')])
+              [Input({'name': MATCH, 'type': 'check_toggle', "list":"entry"}, 'value')])
             #   ,State({'name': MATCH, 'type': 'div'}, 'style'))
 def toggle_checkbox(check_button):
-    print(f">>>>CURRENTLY CHECKSTATUS: {check_button}")
     if len(check_button) > 0:
         # print('SELECTED')  ##uncomment for troubleshooting
         return {'display': 'inline-block', 'width':'30%'}
@@ -135,39 +143,39 @@ def insert_database(data, tab, user='gabri', date=datetime.now().date()):
     db_transact.add_data(tab, data, user)
 
 
-# # get values for all entry fields in current tab --> to be passed on to db in dict form
-# @app.callback(Output('values_to_db', 'children'),
-#               [Input('tabs', 'value')],
-#               [State({'name': ALL, 'type': ALL}, 'value'),
-#               State({'name': ALL, 'type': ALL}, 'id'),
-#               State('values_to_db', 'children')])
-# def send_to_db(tab, values, names, tab_to_db):
-#     # print('TAB TO DB: ', tab_to_db)  ##status of last tab (the one to send data to db for) is returned and saved
-#     if (tab != 'tab-1-example') and (tab_to_db != 'tab-1-example'):
-#         # print("VALUES: ", [x for x in zip(names, values)])  ##uncomment for troubleshooting
-#         info = {n['name']: v for n, v in zip(names, values) if (n['name'].startswith(tab_to_db) and n['type'] != 'div')}  ##get only values when corresponding names contain category name as substring
-#         name = [k for k in info.keys()]
-#         values = [v for k, v in info.items()]
-#         type_ = [n['type'] for n in names if (n['name'].startswith(tab_to_db) and n['type'] != 'div')]  ##use startswith() instead of tab_to_db in n['name'], as 'health' also in 'food_unhealthy_food'
-#         to_db = {}
-#         print(len(name), len(values), len(type_))
-#         if len(name) != len(type_) != len(values):  ##need to be of same length in order to successfully pass data to db
-#                 print("An Error Occured! Lengths of name, value and type lists are unequal!")
-#                 return "ERROR!"
-#         for n, v, t in zip(name, values, type_):  #zip all three if type is 'div' for one - continue (as that will be a div as oppossed to a dcc)
-#             print(f"NAME: {n} - VALUE: {v}")
-#             if (t != 'div'):  #checklists that are not checked return empty list (if <empty_list> - returns False); other entryfields without entry will return None
-#                 entry_name = "_".join(n.split('_')[1:])  ##first part of n is the category --> grab only relevant entry name like used in db
-#                 to_db[entry_name] = True if (('check' in t) and v) else False if (('check' in t) and not v) else None if ((not 'check' in t) and not v) else v
-#                 print(f"{entry_name} : {1 if (('check' in t) and v) else 0 if not v else v}")
+# get values for all entry fields in current tab --> to be passed on to db in dict form
+@app.callback(Output('values_to_db', 'children'),
+              [Input('tabs', 'value')],
+              [State({'name': ALL, 'type': ALL, "list":"entry"}, 'value'),
+              State({'name': ALL, 'type': ALL, "list":"entry"}, 'id'),
+              State('values_to_db', 'children')])
+def send_to_db(tab, values, names, tab_to_db):
+    # print('TAB TO DB: ', tab_to_db)  ##status of last tab (the one to send data to db for) is returned and saved
+    if (tab != 'tab-1-example') and (tab_to_db != 'tab-1-example'):
+        # print("VALUES: ", [x for x in zip(names, values)])  ##uncomment for troubleshooting
+        info = {n['name']: v for n, v in zip(names, values) if n['name'].startswith(tab_to_db)} # and n['type'] != 'div' ##get only values when corresponding names contain category name as substring
+        name = [k for k in info.keys()]
+        values = [v for k, v in info.items()]
+        type_ = [n['type'] for n in names if n['name'].startswith(tab_to_db)]  #and n['type'] != 'div') ##use startswith() instead of tab_to_db in n['name'], as 'health' also in 'food_unhealthy_food'
+        to_db = {}
+        print(len(name), len(values), len(type_))
+        if len(name) != len(type_) != len(values):  ##need to be of same length in order to successfully pass data to db
+                print("An Error Occured! Lengths of name, value and type lists are unequal!")
+                return "ERROR!"
+        for n, v, t in zip(name, values, type_):  #zip all three if type is 'div' for one - continue (as that will be a div as oppossed to a dcc)
+            print(f"NAME: {n} - VALUE: {v}")
+            if (t != 'div'):  #checklists that are not checked return empty list (if <empty_list> - returns False); other entryfields without entry will return None
+                entry_name = "_".join(n.split('_')[1:])  ##first part of n is the category --> grab only relevant entry name like used in db
+                to_db[entry_name] = True if (('check' in t) and v) else False if (('check' in t) and not v) else None if ((not 'check' in t) and not v) else v
+                print(f"{entry_name} : {1 if (('check' in t) and v) else 0 if not v else v}")
 
-#             else:
-#                 continue
-#                 # print(f"Div: {n} - {v}")  ##uncomment for troubleshooting
-#         print(f">>>DATA: {to_db}")
-#         if len(to_db) > 0:
-#             insert_database(to_db, tab_to_db)
-#     return tab
+            else:
+                continue
+                # print(f"Div: {n} - {v}")  ##uncomment for troubleshooting
+        print(f">>>DATA: {to_db}")
+        if len(to_db) > 0:
+            insert_database(to_db, tab_to_db)
+    return tab
 
 # # get date information stored in database
 # @app.callback(Output(None, 'None'),
@@ -178,8 +186,8 @@ def insert_database(data, tab, user='gabri', date=datetime.now().date()):
 #     return date
 
 # get values from date picker
-@app.callback([Output({"name": MATCH ,"type":"picked_date"}, 'children')],
-              [Input({"name": MATCH ,"type":"startdate-input"}, 'date')])
+@app.callback([Output("current_date", 'children')],
+              [Input("date_picker", 'date')])
 def change_date(date):
     print(f'DATE PICKED: {date}')  ##status of last tab (the one to send data to db for) is returned and saved
     date_conv = datetime.strptime(date, '%Y-%m-%d')
@@ -194,21 +202,23 @@ def change_date(date):
         else:
             return ["No data for this date yet!"]
 
-@app.callback(Output({"name": ALL ,"type":ALL}, 'value'),
-              [Input({"name": ALL ,"type":"picked_date"}, 'children')],
-              [State({"name": ALL ,"type":ALL}, 'value'),
-              State({"name": ALL ,"type":ALL}, 'id')])
-def testing(data, values, ids):  #fille entry field values
-    # print("TEST: ", data)
-    if len(set(data)) < 2:
-        print(len(set(data)))
+# @app.callback(Output({"name": ALL ,"type":ALL}, 'value'),
+#               [Input({"name": "selected-date" ,"type":"picked_date"}, 'children')],
+#               [State({"name": ALL ,"type":ALL}, 'value'),
+#               State({"name": ALL ,"type":ALL}, 'id')])
+@app.callback(Output({"name": ALL ,"type":ALL, "list":"entry"}, 'value'),
+              [Input("current_date", 'children')],
+              [State({"name": ALL ,"type":ALL, "list":"entry"}, 'value'),
+              State({"name": ALL ,"type":ALL, "list":"entry"}, 'id')])
+def testing(data, values, ids):  #fill entry field values
+    print(">>>>!!TEST: ", data)
+    if data == "No data for this date yet!":
         return tuple(values)
     else:
-        data = [item for item in data if item != "No data for this date yet!"]
-        print(f">>>>>>DATA to be processed: {len(data)}")
+        data_dict = ast.literal_eval(data)   ##convert data string into dict
+        print(">>>>!!TEST: ", data_dict)
+        print(f">>>>>>DATA to be processed: {len(data_dict)} for data of type {type(data_dict)}")
         values_for_date = []
-        data_str = data[0]  ##convert data (saved in list) to string 
-        data_dict = ast.literal_eval(data_str)   ##convert data string into dict
         print(f"\nDATA (testing): {data_dict}\n")
 
         for id_, val in zip(ids, values):  #extract cotegory, entry_name and entry_type for each entry_id
@@ -220,17 +230,29 @@ def testing(data, values, ids):  #fille entry field values
             else:
                 category = id_["name"].split("_")[0]
                 entry_name = "_".join(id_["name"].split("_")[1:])
+                print(entry_name)
+                try:
+                    db_val = data_dict[category][entry_name]
+                except:
+                    values_for_date.append(val)
+                    continue
                 if ('check' in entry_type):
-                    current_val = [entry_name] if data_dict[category][entry_name]==True else []
+                    try:
+                        current_val = [entry_name] if db_val==True else []
+                    except:
+                        current_val = val
                     values_for_date.append(current_val)
                 elif (entry_type == "permanent"):
-                    current_val = data_dict[category][entry_name]
+                    current_val = db_val
                     values_for_date.append(current_val)
                 elif entry_type == "on_demand":
                     caller_name = "_".join(entry_name.split("_")[:-1])
-                    caller_value = data_dict[category][caller_name]
+                    try:
+                        caller_value = data_dict[category][caller_name]
+                    except:
+                        caller_value = None
                     if caller_value:
-                        current_val = data_dict[category][entry_name]
+                        current_val = db_val
                         values_for_date.append(current_val)
                     else:
                         values_for_date.append(None)
