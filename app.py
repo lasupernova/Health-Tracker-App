4a31139614dd6d0442b7299dc4d6535da5c6e017
lasupernova <gabrilasuno@gmail.com>
2021-05-15 22:15:16 +0200

    fixed bug in db_vals_to_list(): empty values for non-checklist entry fields changed from None to '', as None would not remove entry from previous date

--- app.py
+++ app.py
@@ -46,19 +46,27 @@ app.layout = html.Div([
                 dcc.Tab(id='longterm_tab', label='Longterm', value='longterm'),
             ],  vertical=False, parent_style={'float': 'left'})], width = 9),
             dbc.Col([
-                        dcc.DatePickerSingle(
-                            id="date_picker",
-                            date=datetime.today().date()),
-                        html.Div(id="current_date", children=["BLUB"]),
-                        html.Div(id="db_data", children=[], style={'display': 'none'})
-                        ], width = "auto"),
+                    dcc.DatePickerSingle(
+                        id="date_picker",
+                        date=datetime.today().date()),
+                    html.Div(id="current_date", children=["BLUB"]),
+                    html.Div(id="previous_date"),  #, hidden=True   #division saving previous date to save last tab to db (-> no tab change necessary for saving data in final tab before switching dates)
+                    html.Div(id="db_data", children=[], style={'display': 'none'})
+                    ], width = "auto"),
 
 
         ]),
     html.Div(id="values_to_db", children=['None'], style={'display':'block'}),
+    html.Div(id="values_to_db2", children=['None'], hidden=True),
     html.Div(id="test", children=['None'], style={'display':'block'})
     ])
 ])
+
+@app.callback([Output('hover_test', 'title')], 
+               Input('hover_test', 'title'))
+def testHover(hoverdata):
+    print(hoverdata)
+    return ["SUCCESS!!"]
 
 @app.callback([Output('mood_tab', 'children'),
                Output('health_tab', 'children'),
@@ -116,15 +124,8 @@ def insert_database(data, tab, date, user='gabri'):
     # insert into database
     db_transact.add_data(tab, data, user)
 
-
-# get values for all entry fields in current tab --> to be passed on to db in dict form
-@app.callback(Output('values_to_db', 'children'),
-              [Input('tabs', 'value')],
-              [State({'name': ALL, 'type': ALL, "list":"entry"}, 'value'),
-              State({'name': ALL, 'type': ALL, "list":"entry"}, 'id'),
-              State('values_to_db', 'children'),
-              State('date_picker', 'date')])
 def send_to_db(tab, values, names, tab_to_db, date):
+    print(">>>>>>>>>>>>>>>>>>>> RUNNING!!!!!!! <<<<<<<<<<<<<<<<<<")
     # print('TAB TO DB: ', tab_to_db)  ##status of last tab (the one to send data to db for) is returned and saved
     if (tab != 'tab-1-example') and (tab_to_db != 'tab-1-example'):
         # print("VALUES: ", [x for x in zip(names, values)])  ##uncomment for troubleshooting
@@ -163,30 +164,73 @@ def send_to_db(tab, values, names, tab_to_db, date):
     return tab
 
 
+# get values for all entry fields in current tab --> to be passed on to db in dict form
+@app.callback(Output('values_to_db', 'children'),
+              [Input('tabs', 'value')],
+              [State({'name': ALL, 'type': ALL, "list":"entry"}, 'value'),
+              State({'name': ALL, 'type': ALL, "list":"entry"}, 'id'),
+              State('values_to_db', 'children'),
+              State('date_picker', 'date')])
+def to_db_from_current_date(tab, values, names, tab_to_db, date):
+    print(f">>>>>>INITIAL TAB: {tab}")
+    return send_to_db(tab, values, names, tab_to_db, date)
+
+# get values for all entry fields in current tab --> to be passed on to db in dict form (without tab change, but triggered by date change)
+@app.callback([Output('values_to_db2', 'children'),
+               Output('previous_date', 'children')],
+              [Input('date_picker', 'date')],
+              [State({'name': ALL, 'type': ALL, "list":"entry"}, 'value'),
+              State({'name': ALL, 'type': ALL, "list":"entry"}, 'id'),
+              State('values_to_db', 'children'),
+              State('previous_date', 'children'),
+              State('tabs', 'value')])
+def to_db_from_previous_date(date, values, names, tab_to_db, prev_date, tab):
+    print(f">>>>>>>>>>>>>> DATE FROM PREVIOUS: {date}")
+    if prev_date:
+        return send_to_db(tab, values, names, tab_to_db, prev_date), date
+    else:
+        print(f"to_db_from_previous_date: no action as prev_date is {prev_date}")
+        return tab, date
+
+
 # get values from date picker
 @app.callback([Output("current_date", 'children'),
                Output("db_data", 'children')],
               [Input("date_picker", 'date')])
-def change_date(date):
-    print(f'DATE PICKED: {date}')  ##status of last tab (the one to send data to db for) is returned and saved
-    date_conv = datetime.strptime(date, '%Y-%m-%d')
-    print(date_conv)
-    if date == datetime.today().date():
-        return ["TODAY"]
+def change_date(date_):
+    """
+    Function triggered by callback for date-picker date change.
+    Date needs to be converted from string to datetime object in order to be passed on to
+    database query.
+    Values for new date and specified user are retrieved and
+    return retrieved data and a status message that is printed to dashboard.
+
+    Parameters:
+        date - str: date selected from date-picker in string format
+
+    Returns:
+        status message - str: message (data loaded/no data for this date) passed to "current_date" html.Div
+        data - str: returned data is passed to hidden db_data html.Div children, which in turn triggers follow-up callback
+        (NOTE: str(data) instead od data returned as a workaround, as children of html.Div-objects can NOT be objects, but need to be strings)
+    """
+    ##status of last tab (the one to send data to db for) is returned and saved
+    date_conv = datetime.strptime(date_, '%Y-%m-%d')
+
+    data = db_transact.query_data_by_date_and_user(date_conv, user='gabri', end_date=None)
+    print(f">>>>DATE (change_date): {data}")
+    if data != 2:
+        return "Data for selected date loaded", str(data)
     else:
-        data = db_transact.query_data_by_date_and_user(date_conv, user='gabri', end_date=None)
-        print(f">>>>DATE (change_date): {data}")
-        if data != 2:
-            return "Data for selected date loaded", str(data)
-        else:
-            return "No data for this date yet!", "No data for this date yet!"
+        return "No data for this date yet!", "No data for this date yet!"
+
 
 def db_vals_to_list(data_dict, values, ids):
     """
     Iterates over current entry values and replaces each value:
     with db value (if entry available in db for specified date and user) 
     or 
-    [] (checklist entry fields) or None (non-checklist entry fields) if no value returned from db.
+    [] (checklist entry fields) or "" (non-checklist entry fields) if no value returned from db.
+    NOTE: "" and not None used for empty value in non-checkfields, as None would not remove entry from previous date in entryfield
 
     Parameters:
         data - dict: dict of values returned from db for each entry per category
@@ -207,26 +251,25 @@ def db_vals_to_list(data_dict, values, ids):
 
         # check if value was returned from db for current entry, otherwise append empty value (depending on entry type)
         try:
-            db_val = data_dict[category][entry_name]
+            db_val = data_dict[category][entry_name] if data_dict[category][entry_name] else ""
+            print(f"{entry_name}: {db_val}")
+            # multiple values (per entry) are returned from db as string with values surrounded by "{}" --> convert to list with individual values
             if type(db_val)==str and "{" in db_val:
                 db_val = db_val.strip("{}")
-                print(f"STRIPPED: {db_val}")
                 db_val = db_val.replace('"', '').split(",")
-                print(f"List db_val: {db_val}")
         except:
-            empty_val = [] if "check" in entry_type else None
+            empty_val = [] if "check" in entry_type else ""
             values_for_date.append(empty_val)
             continue
 
         if ('check' in entry_type):
-            # try:
             current_val = [entry_name] if db_val==True else []
-            # except:
-            #     current_val = val
             values_for_date.append(current_val)
+
         elif (entry_type == "permanent"):
             values_for_date.append(db_val)
-        elif entry_type == "on_demand":
+        
+        elif entry_type == "on_demand":  #only fill on-demand fields if correspongding caller was checked
             caller_name = "_".join(entry_name.split("_")[:-1])
             try:
                 caller_value = data_dict[category][caller_name]
@@ -235,23 +278,39 @@ def db_vals_to_list(data_dict, values, ids):
             if caller_value:
                 values_for_date.append(db_val)
             else:
-                values_for_date.append(None)
+                values_for_date.append("")  ##HERE
     return values_for_date
 
 @app.callback(Output({"name": ALL ,"type":ALL, "list":"entry"}, 'value'),
               [Input("db_data", 'children')],
               [State({"name": ALL ,"type":ALL, "list":"entry"}, 'value'),
               State({"name": ALL ,"type":ALL, "list":"entry"}, 'id')])
-def testing(data, values, ids):  #fill entry field values
+def fill_entryfields(data, values, ids):  #fill entry field values
+    """
+    Fills entry fields with values based on current date (as per date-picker) and user.
+    Correct entry field order is determined by taking in all entryfield ids (+values),
+    iterating over these and assign the values retrieved from db to them by returning 
+    a tuple of values in the correct order.
+    Database values are passed as "data" parameter, which is a dict in string form
+    -> this data string is converted back to dict-format using as.literal_eval().
+
+    Parameters:
+        data - str: data retrieved from db as a dict in string form
+        values - list: list of current entry field values 
+        ids - list: list of entry field ids
+
+    Returns:
+        values_for_date/vals - list: list of retrieved values in correct order and format for corresponding entry fields
+    """
     if data == "No data for this date yet!":
         vals = [[] if "check" in entry_type["type"] else None for entry_type in ids]
-        return tuple(vals)
+        return vals
     else:
         data_dict = ast.literal_eval(data)   ##convert data string into dict
 
         values_for_date = db_vals_to_list(data_dict, values, ids)
-        print(f">>>>RESULTS: {tuple(values_for_date)} number of return vals {len(values_for_date)}")
-        return tuple(values_for_date)
+        print(f">>>>RESULTS: {values_for_date} number of return vals {len(values_for_date)}")
+        return values_for_date
 
 
 if __name__ == '__main__':
